7ade2af1800dcc1d86bc510d434a4a837a3094e5
Chico Bell <chico.bell@justdevelop.it>
2021-09-06 17:53:49 +0100

    Added setting to send log files

--- src/main/java/com/protectednet/utilizr/logging/LogcatLogger.kt
+++ src/main/java/com/protectednet/utilizr/logging/LogcatLogger.kt
@@ -2,8 +2,10 @@ package com.protectednet.utilizr.logging
 
 import android.content.Context
 import android.os.Environment
+import android.util.Log
 import java.io.File
 import java.io.IOException
+import java.lang.Exception
 import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.concurrent.thread
@@ -15,11 +17,10 @@ object LogcatLogger {
 
     /*How often logcat should dump to file*/
     var isLogging = false
-    var context: Context? = null
     lateinit var logDirectory:File
     private var process: Process? = null
-    private lateinit var logFile: File
-
+    private var logFile: File? = null
+    var flushInitiated =false
     /* Checks if external storage is available for read and write */
     fun isExternalStorageWritable(): Boolean {
         val state = Environment.getExternalStorageState()
@@ -38,32 +39,40 @@ object LogcatLogger {
         logDirectory = File("$appDirectory/$folder")
         val formatter = SimpleDateFormat("dd_MM_yyyy", Locale.getDefault())
         val date = formatter.format(Date(System.currentTimeMillis()))
-
         // create app folder
         if (!appDirectory.exists()) {
             appDirectory.mkdir()
         }
-
         // create log folder
         if (!logDirectory.exists()) {
             logDirectory.mkdir()
         }
-
-
         //do one file per day
         logFile = File(logDirectory.absolutePath, "logcat$date.txt")
-//            if(!logFile.exists())
-//                logFile.createNewFile()
-        thread {
-            if (!logDirectory.exists())
-                return@thread
-            val files = logDirectory.listFiles()
-            if (files != null && files.isNotEmpty())
-                for (f in files) {
-                    if (System.currentTimeMillis() - f.lastModified() > 6 * 60 * 60 * 1000)//delete if more than 6 hours old
+        flushLogs()
+    }
+
+    /**
+    This method should be called at intervals in-case the logs are heavy and taking up spaces.
+    A 2-hourly interval is advised
+     */
+    fun flushLogs() {//logs can grow quite heave so clear regularly
+        if(logFile == null) return
+        if (!logDirectory.exists())
+            return
+        val files = logDirectory.listFiles()
+        if (files != null && files.isNotEmpty())
+            for (f in files) {
+                try {
+                    if (f.path != logFile?.path
+                        || System.currentTimeMillis() - f.lastModified() > 6 * 60 * 60 * 1000
+                        || f.length() > 10000000L
+                    )
                         f.delete()
+                } catch (e: Exception) {
+                    Log.e("DeleteLog", e.message ?: "")
                 }
-        }
+            }
     }
 
 
@@ -71,7 +80,7 @@ object LogcatLogger {
         // clear the previous logcat and then write the new one to the file
         try {
             process = Runtime.getRuntime().exec("logcat -c")
-            val command = "logcat -f ${logFile.absolutePath} --pid ${android.os.Process.myPid()} *:D"
+            val command = "logcat -f ${logFile?.absolutePath} --pid ${android.os.Process.myPid()} *:D"
             process = Runtime.getRuntime().exec(command)
             isLogging = true
         } catch (e: IOException) {
