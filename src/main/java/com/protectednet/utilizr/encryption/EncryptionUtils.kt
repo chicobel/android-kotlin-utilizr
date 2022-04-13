3562f0e4d15ce5971bf6b3658dc2e1174f57568a
Chico Bell <chico.bell@justdevelop.it>
2022-04-13 16:26:04 +0100

    Switched VPN to use avira Get the config and auth details from the protected api

---
+++ src/main/java/com/protectednet/utilizr/encryption/EncryptionUtils.kt
@@ -0,0 +1,56 @@
+package com.protectednet.utilizr.encryption
+
+import java.io.UnsupportedEncodingException
+import java.math.BigInteger
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
+import kotlin.experimental.and
+
+object EncryptionUtils {
+
+    fun toSHA1(input: String): String {
+        return try {
+            val md = MessageDigest.getInstance("SHA-1")
+            val messageDigest = md.digest(input.toByteArray())
+            val no = BigInteger(1, messageDigest)
+            var hashtext = no.toString(16)
+            while (hashtext.length < 32) {
+                hashtext = "0$hashtext"
+            }
+            hashtext
+        } catch (e: NoSuchAlgorithmException) {
+            throw RuntimeException(e)
+        }
+    }
+
+    fun toSHA256(input: String): String {
+        return try {
+            val md = MessageDigest.getInstance("SHA-256")
+            val messageDigest = md.digest(input.toByteArray())
+            val no = BigInteger(1, messageDigest)
+            var hashtext = no.toString(16)
+            while (hashtext.length < 32) {
+                hashtext = "0$hashtext"
+            }
+            hashtext
+        } catch (e: NoSuchAlgorithmException) {
+            throw RuntimeException(e)
+        }
+    }
+
+    fun toSHA512(input: String): String {
+        return try {
+            val md = MessageDigest.getInstance("SHA-512")
+            val messageDigest = md.digest(input.toByteArray())
+            val no = BigInteger(1, messageDigest)
+            var hashtext = no.toString(16)
+            while (hashtext.length < 32) {
+                hashtext = "0$hashtext"
+            }
+            hashtext
+        } catch (e: NoSuchAlgorithmException) {
+            throw RuntimeException(e)
+        }
+    }
+
+}
